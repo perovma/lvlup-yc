a38d6b7ba872251f5ffdb7181cf02956e7e1bc23
Perov Mikhail <duxatomik@gmail.com>
2022-12-04 22:28:03 +0300

    add ingress config

--- app/app.py
+++ app/app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python"
+    return "Hello from Python - mperov simple web-app was running"
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
